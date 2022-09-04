cbb9090f2cf745d76379a197c817619fa2dd157a
itsmarsss <itsmarzzzzzz@gmail.com>
2022-09-04 18:17:24 -0400

    Update Browse JS

--- js/browse.js
+++ js/browse.js
@@ -2,6 +2,8 @@ const sortby = document.getElementById("sortby");
 const search = document.getElementById("search");
 const results = document.getElementById("results");
 
+const list = document.getElementById("list");
+
 const min = document.getElementById("min");
 const max = document.getElementById("max");
 
@@ -23,4 +25,40 @@ function updateResults() {
 
 function searchItem() {
     results.innerHTML = 'Results for "' + search.value + '"'
+
+    var xhr = new XMLHttpRequest();
+    var url = "api/Sellers/GetBySearch/";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(`{"term": "${search.value}"}`));
+    const re = xhr.responseText;
+    const results = JSON.parse(re);
+
+    for (let i = 0; i < results.length; i++) {
+        const item = results[i];
+
+        const id = item.Id;
+        const type = item.Type;
+        const name = item.Name;
+        const seller = item.Seller;
+        const image = item.Image;
+        const condition = item.Condition;
+        const price = item.Price;
+
+        const card =
+            `
+        <div class="item-card" id="${id}">
+            <div class="left">
+                <img src="${image}" alt="Item Image">
+            </div>
+            <div class="right">
+                <h5>${name}</h5>
+                <h6>${type + " - " + seller}</h6>
+                <h6>${condition}</h6>
+                <h7>${price}</h7>
+            </div>
+        </div>
+        `;
+        list.innerHTML += card;
+    }
 }
