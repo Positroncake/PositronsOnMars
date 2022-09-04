1166596f0f7e93effca2e523bfb080fe0119605b
itsmarsss <itsmarzzzzzz@gmail.com>
2022-09-04 00:21:48 -0400

    Update featured page

---
+++ js/featured.js
@@ -0,0 +1,21 @@
+const featured1 = document.getElementById("f1");
+const featured2 = document.getElementById("f2");
+const featured3 = document.getElementById("f3");
+
+featured1.addEventListener("click", (event) => {
+    featured1.classList.add('selected');
+    featured2.classList.remove('selected');
+    featured3.classList.remove('selected');
+});
+
+featured2.addEventListener("click", (event) => {
+    featured2.classList.add('selected');
+    featured1.classList.remove('selected');
+    featured3.classList.remove('selected');
+});
+
+featured3.addEventListener("click", (event) => {
+    featured3.classList.add('selected');
+    featured2.classList.remove('selected');
+    featured1.classList.remove('selected');
+});
