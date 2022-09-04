5258abfd046674e989729090cf354c0602cf676a
itsmarsss <itsmarzzzzzz@gmail.com>
2022-09-04 19:48:13 -0400

    Add dummy cards

--- js/browse.js
+++ js/browse.js
@@ -6,7 +6,7 @@ const list = document.getElementById("list");
 const min = document.getElementById("min");
 const max = document.getElementById("max");
 
-let sort = "Newest";
+let sort = "high to low";
 let cate = "All";
 
 function show(cate2) {
@@ -23,52 +23,53 @@ function updateResults() {
 }
 
 function searchItem() {
-    const status = document.getElementById("status");
-    status.innerHTML = 'Results for "' + search.value.substring(0,25) + (search.value.length > 25 ? '..."' : '"');
+    if (search.value.length > 0) {
+        const status = document.getElementById("status");
+        status.innerHTML = 'Results for "' + search.value.substring(0, 25) + (search.value.length > 25 ? '..."' : '"');
 
-    var xhr = new XMLHttpRequest();
-    var url = "http://172.105.105.74:55555/api/Sellers/GetBySearch";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(`${search.value}`));
-    const re = xhr.responseText;
-    const results = JSON.parse(re);
+        var xhr = new XMLHttpRequest();
+        var url = "http://172.105.105.74:55555/api/Sellers/GetBySearch";
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(`${search.value}`));
+        const re = xhr.responseText;
+        const results = JSON.parse(re);
 
-    for (let i = 0; i < results.length; i++) {
-        const item = results[i];
+        for (let i = 0; i < results.length; i++) {
+            const item = results[i];
 
-        const id = item.Id;
-        const type = item.Type;
+            const id = item.Id;
+            const type = item.Type;
 
-        let strtype = "Unknown";
-        if(type == 1){
-            strtype = "CPU";
-        } else if(type == 2){
-            strtype = "GPU";
-        } else if(type == 3){
-            strtype = "RAM";
-        } else if(type == 4){
-            strtype = "Motherboard";
-        } else if(type == 5){
-            strtype = "Storage";
-        } else if(type == 6){
-            strtype = "PSU";
-        }
+            let strtype = "Unknown";
+            if (type == 1) {
+                strtype = "CPU";
+            } else if (type == 2) {
+                strtype = "GPU";
+            } else if (type == 3) {
+                strtype = "RAM";
+            } else if (type == 4) {
+                strtype = "Motherboard";
+            } else if (type == 5) {
+                strtype = "Storage";
+            } else if (type == 6) {
+                strtype = "PSU";
+            }
 
-        const name = item.Name;
-        const seller = item.Seller;
-        const image = item.Image;
-        const condition = item.Condition;
+            const name = item.Name;
+            const seller = item.Seller;
+            const image = item.Image;
+            const condition = item.Condition;
 
-        let strcond = condition + " ";
-        for(let j = 0; j < condition; j++){
-            strcond += `<i style="color: gold;" class="fa fa-star"></i>`
-        }
+            let strcond = condition + " ";
+            for (let j = 0; j < condition; j++) {
+                strcond += `<i style="color: gold;" class="fa fa-star"></i>`
+            }
 
-        const price = item.Price;
+            const price = item.Price;
 
-        const card =
-            `
+            const card =
+                `
         <div class="item-card" id="${id}">
             <div class="left">
                 <img src="${image}" alt="Item Image">
@@ -81,8 +82,9 @@ function searchItem() {
             </div>
         </div>
         `;
-        if (cate.includes(type)) {
-            list.innerHTML += card;
+            if (cate.includes(type)) {
+                list.innerHTML += card;
+            }
         }
     }
 }
